6ad219a14d1c27610e32cc0315e2cbd0766f80a9
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-11-03 08:50:44 +0300

    google page

---
+++ app/src/main/java/com/example/google_page/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.google_page
+
+import android.content.Intent
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.google_page.R
+import com.example.google_page.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//
+//        mainButton.setOnClickListener{
+//            val intent = Intent(this,activity2::java)
+//            startActivity(intent)
+//       }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mainButton.setOnClickListener { CheckInfo() }
+
+    }
+}
+    private fun CheckInfo() {
+
+
+        val first = binding.firstnameEdit.text.toString()
+        val last = binding.lastnameEdit.text.toString()
+        val email = binding.emailEdit.text.toString()
+        val pass = binding.pass1Edit.text.toString()
+        val pass2 = binding.pass2Edit.text.toString()
+        val password = pass == pass2
+
+        val check = !(email.contains("!") &&  email.contains("#") && email.contains("-")
+                && email.contains("(") && email.contains(")")&& email.contains("="))
+
+        var information =
+            if (first.isNotEmpty() && (email.contains("@") && email.contains(".com"))&& last.isNotEmpty() && pass.isNotEmpty() && pass2.isNotEmpty() && check) {
+"info"
+
+            } else if (!password) {
+                "ERROR!!!\nPlease enter password "
+
+
+            } else {
+
+                "ERROR!!!\nPlease enter all the information"
+
+
+            }
+
+
+        binding.finalResult.text = information
+    }
+
+
+
